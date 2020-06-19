c47ad7d77a11c64bf871b9722e29020f59b0832f
robertarfa <roberta_rfa@hotmail.com>
2020-06-18 22:53:12 -0300

    Menu resposiviness

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import css from './menu.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Col, Row } from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
@@ -13,43 +12,42 @@ const Menu = () => {
   return (
     <main>
       <section>
-        <Row className={css.menuDiv}>
-          <Col xs={3}>
+        <Navbar expand="md">
+          <Navbar.Brand>
             <header className={css.logoDiv}>
               <img src={logo} alt="CollabTranslation" />
             </header>
-          </Col>
+          </Navbar.Brand>
 
-          <Col sm={true}>
-            <Navbar expand="sm">
-              <Nav className="xs-auto">
-                <Nav.Link className={css.link} href="/">
-                  Home /
-                </Nav.Link>
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <Nav className="xs-auto">
+              <Nav.Link className={css.link} href="/">
+                Home /
+              </Nav.Link>
 
-                <Nav.Link className={css.link} href="/Services">
-                  Services /
-                </Nav.Link>
+              <Nav.Link className={css.link} href="/Services">
+                Services /
+              </Nav.Link>
 
-                <Nav.Link className={css.link} href="/Prices">
-                  Prices & Combos /
-                </Nav.Link>
+              <Nav.Link className={css.link} href="/Prices">
+                Prices & Combos /
+              </Nav.Link>
 
-                <Nav.Link className={css.link} href="/AboutUs">
-                  About Us /
-                </Nav.Link>
+              <Nav.Link className={css.link} href="/AboutUs">
+                About Us /
+              </Nav.Link>
 
-                <Nav.Link className={css.link} href="/Associations">
-                  Associations & Certificates /
-                </Nav.Link>
+              <Nav.Link className={css.link} href="/Associations">
+                Associations & Certificates /
+              </Nav.Link>
 
-                <Nav.Link className={css.link} href="/Terms">
-                  Terms & Conditions
-                </Nav.Link>
-              </Nav>
-            </Navbar>
-          </Col>
-        </Row>
+              <Nav.Link className={css.link} href="/Terms">
+                Terms & Conditions
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </section>
     </main>
   );
