f93d876c8971976c512804d19ffee87ea026cdf4
robertarfa <roberta_rfa@hotmail.com>
2020-06-19 21:46:47 -0300

    add material-icons

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 //import './styles.css';
 import Menu from '../../components/Menu/Menu';
 import HorizontalPanel from '../../components/HorizontalPanel/HorizontalPanel';
+import VerticalPanels from '../../components/VerticalPanels/VerticalPanels';
 
 const Home = () => {
   return (
     <div>
       <Menu />
       <HorizontalPanel />
+      <VerticalPanels />
     </div>
   );
 };
