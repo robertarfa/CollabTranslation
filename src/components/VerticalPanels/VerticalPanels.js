79e8cae83d04830a04d842c2bc8821fd4a1f15f0
robertarfa <roberta_rfa@hotmail.com>
2020-06-21 17:34:52 -0300

    refactoring vertical panel component

---
+++ src/components/VerticalPanels/VerticalPanels.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import css from './verticalPanels.module.css';
+
+export default function VerticalPanels(props) {
+  const {
+    titleCard,
+    paragraph,
+    backgroudColor,
+    iconColor,
+    icon,
+    linkPage,
+  } = props;
+
+  return (
+
+    <div className={css.myCard} style={{ background: backgroudColor }}>
+      <div>
+        <span
+          className="material-icons"
+          style={{ fontSize: '4em', color: iconColor }}
+        >
+          {icon}
+        </span>
+        <h4 className={css.litleTitle}>{titleCard}</h4>
+        <p>{paragraph}</p>
+        <a href={linkPage}>Read More {'>'}</a>
+      </div>
+    </div>
+
+
+  );
+}
