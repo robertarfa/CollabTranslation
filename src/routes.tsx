b43924e6958d4e76677d8155fbb459bc255551c5
Roberta Aguilar <roberta.aguilar@kobel.com.br>
2020-11-03 09:07:21 -0300

    Switch

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, BrowserRouter } from 'react-router-dom'
+import { Route, BrowserRouter, Switch } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Prices from './pages/Prices'
@@ -7,16 +7,19 @@ import Services from './pages/Services'
 import About from './pages/AboutUs'
 import Associations from './pages/Associations'
 import Terms from './pages/Terms'
+// import Switch from 'react-bootstrap/esm/Switch'
 
 const Routes = () => {
   return (
     <BrowserRouter>
+      <Switch>
       <Route component={Home} path="/" exact />
       <Route component={Services} path="/Services" />
       <Route component={Prices} path="/Prices" />
       <Route component={About} path="/AboutUs" />
       <Route component={Associations} path="/Associations" />
-      <Route component={Terms} path="/Terms" />
+        <Route component={Terms} path="/Terms" />
+        </Switch>
     </BrowserRouter>
   )
 }
